23da33a9c4fe1b051fc72a8001135af69bb15d82
maazcw <maaz.shaikh@onlinpsbloans.com>
2023-05-10 12:33:25 +0530

    Initial commit

---
+++ src/main/java/com/opl/jns/registry/lib/serviceregistrylibjns/ServiceRegistryLibJnsApplication.java
@@ -0,0 +1,13 @@
+package com.opl.jns.registry.lib.serviceregistrylibjns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryLibJnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryLibJnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opl/jns/registry/lib/serviceregistrylibjns/ServiceRegistryLibJnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opl.jns.registry.lib.serviceregistrylibjns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryLibJnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
